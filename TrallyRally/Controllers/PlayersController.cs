93274d5b0f244ac62ee6adc4a386235aee27a262
Emilis Baliukonis <emilisb@wix.com>
2021-05-10 18:05:40 +0300

    Basic working players controller

---
+++ TrallyRally/Controllers/PlayersController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrallyRally.Data;
+using TrallyRally.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TrallyRally.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public PlayersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Players
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Players.ToListAsync());
+        }
+
+        // GET: Players/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Phone,Avatar,StartPosition,StartTime,Password")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                player.Password = new PasswordHasher<Player>().HashPassword(player, player.Password);
+                _context.Add(player);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Phone,Avatar,StartPosition,StartTime,Password")] Player player)
+        {
+            if (id != player.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (player.Password == null)
+                {
+                    var currentPlayer = await _context.Players.AsNoTracking().FirstAsync(x => x.ID == id);
+                    player.Password = currentPlayer.Password;
+                }
+                else
+                {
+                    player.Password = new PasswordHasher<Player>().HashPassword(player, player.Password);
+                }
+
+                try
+                {
+                    _context.Update(player);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerExists(player.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var player = await _context.Players.FindAsync(id);
+            _context.Players.Remove(player);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerExists(int id)
+        {
+            return _context.Players.Any(e => e.ID == id);
+        }
+    }
+}
